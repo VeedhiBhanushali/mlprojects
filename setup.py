d1fed6583446f93f7778a30279ca3171bcb972de
VeedhiBhanushali <bhanushaliveedhi@gmail.com>
2025-03-01 18:48:48 -0800

    EDA and Problem Statements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='mlprojects',
 version='0.0.1',
 author='Veedhi Bhanushali',
 author_email='bhanushaliveedhi@gmail.com',
